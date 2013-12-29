a907696e18f1ed0cb83bc48a5e16e38c8c879952
Ali <aliasgar@outlook.com>
2013-12-29 20:05:56 +0530

    changed the gravatar and updated the about section

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-/* globals require:true, exports:true */
+/* globals require:true, exports:true, module:true */
 /**
  * GET home page.
  */
@@ -7,30 +7,46 @@
 var crypto = require('crypto');
 var qs = require('querystring');
 
+function init(app) {
 
-exports.index = function (req, res) {
 
-    var params = {
-        s: 896
+    exports.index = function (req, res) {
+
+        var params = {
+            s: 896
+        };
+
+        res.render('index', {
+            title: 'Rushil Kekre',
+            src: {
+                main: getImageSrc('rushil.kekre@outlook.com', params),
+                secondary: getImageSrc('aliroks@gmail.com', params)
+            }
+        });
     };
 
-    res.render('index', {
-        title: 'Rushil Kekre',
-        src: {
-            main: getImageSrc('aliasgar@outlook.com', params),
-            secondary: getImageSrc('aliroks@gmail.com', params)
-        }
-    });
-};
+    /**
+     *
+     * @param {String} email
+     * @param {object} params
+     * @param {boolean} [doNotCache] Default = True if env is dev. If set to true, adds a timestamp to the url to prevent browser from using cached image
+     * @returns {string} the image source
+     */
+    function getImageSrc(email, params, doNotCache) {
 
-/**
- *
- * @param {String} email
- * @param {object} params
- * @returns {string} the image source
- */
-function getImageSrc(email, params) {
-    var hash = crypto.createHash('md5').update(email.toLowerCase()).digest('hex');
-    var querystring = qs.encode(params);
-    return 'https://s.gravatar.com/avatar/' + hash + '?' + querystring;
+        if (!email) {
+            return null;
+        }
+
+        if (doNotCache || app.get('env') === 'development') {
+            params.current_time = new Date().getTime();
+        }
+
+        var hash = crypto.createHash('md5').update(email.toLowerCase()).digest('hex');
+        var querystring = qs.encode(params);
+        return 'https://s.gravatar.com/avatar/' + hash + '?' + querystring;
+    }
+
 }
+
+module.exports = init;
